ef8ab35d52d3c2d82a09937114d65bd5273e83d0
zhangpeng <zhang18257@163.com>
2021-06-20 11:37:52 +0800

    hotfix update

--- hello.java
+++ hello.java
@@ -10,5 +10,5 @@ say helloi love china!
 say helloi love china!
 say helloi love china!
 say helloi love china!
-say helloi love china!
-say helloi love china!  jhskhfa
+say helloi love china!  hotfix update
+say helloi love china!  jhskhfa update
